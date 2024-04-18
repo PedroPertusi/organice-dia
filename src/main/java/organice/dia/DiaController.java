1540ba42958fdb89dc161e7607b32416153aff11
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2024-04-18 09:07:17 -0300

    Revertendo

--- src/main/java/organice/dia/DiaController.java
+++ src/main/java/organice/dia/DiaController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-import organice.lembrete.LembreteDateIn;
 import organice.lembrete.LembreteOut;
 
 
@@ -21,13 +20,13 @@ public interface DiaController {
 
     // Rota de leitura 
     @GetMapping("/dia/{id_dia}")
-    ResponseEntity<DiaOut> read(@PathVariable(required = true) String id_dia);
+    ResponseEntity<DiaOut> read(@PathVariable("id_dia") String id_dia);
 
     // Rota de leitura de lembretes
     @PostMapping("/dia/lembretes")
     ResponseEntity<List<LembreteOut>> read_lembretes(
         @RequestHeader(required = true, name = "id-user") String UserId,    
-        @RequestBody LembreteDateIn data);
+        @RequestBody DiaData data);
 
     // Rota de criação
     @PostMapping("/dia")
@@ -37,11 +36,11 @@ public interface DiaController {
 
     // Rota de atualização
     @PutMapping("/dia/{id_dia}")
-    ResponseEntity<DiaOut> update(@PathVariable(required = true) String id_dia, @RequestBody DiaIn diaIn);
+    ResponseEntity<DiaOut> update(@PathVariable("id_dia") String id_dia, @RequestBody DiaIn diaIn);
 
     // Rota de exclusão
     @DeleteMapping("/dia/{id_dia}")
-    ResponseEntity<Void> delete(@PathVariable(required = true) String id_dia);
+    ResponseEntity<Void> delete(@PathVariable("id_dia") String id_dia);
 
 
 } 
